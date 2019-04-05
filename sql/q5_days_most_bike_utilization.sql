f14658e40220da6fcdd67ad53a4b3d88c7f8d729
SergeyKulagin <kulaginsp@gmail.com>
2019-04-05 20:36:59 +0300

    sql: q5

--- sql/q5_days_most_bike_utilization.sql
+++ sql/q5_days_most_bike_utilization.sql
@@ -3,20 +3,15 @@ WITH RECURSIVE
 		SELECT CAST(MIN(STRFTIME('%s', datetime(trip.start_time))) AS INT) FROM trip
 		UNION
 				SELECT x + 60*60*24 FROM days_range WHERE x <= (SELECT MAX(STRFTIME('%s', datetime(trip.end_time))) FROM trip)
-																
---		SELECT x + 60*60*24 FROM days_range WHERE x <=  
 	)
-SELECT DATETIME(dr.x, 'unixepoch') as 'day' from days_range dr;
-
-
-/*SELECT
-	STRFTIME('%Y', datetime(t.start_time)) as 'year_start',
-	STRFTIME('%Y', datetime(t.end_time)) as 'year_end',
-	STRFTIME('%m', datetime(t.start_time)) as 'month_start',
-	STRFTIME('%m', datetime(t.end_time)) as 'month_end',
-	t.start_time,
-	t.end_time,
-	(STRFTIME('%s', datetime(t.end_time))- STRFTIME('%s', datetime(t.start_time)))/ 60
-FROM
-	trip t
-WHERE month_start != month_end or year_start != year_end*/
+SELECT 
+DATE(dr.x, 'unixepoch'),
+STRFTIME('%s', DATE(dr.x, 'unixepoch')) as day_start, 
+STRFTIME('%s', DATE(dr.x, 'unixepoch', '+1 day')) as day_end,
+STRFTIME('%s', t.start_time) as trip_start,
+STRFTIME('%s', t.end_time) as trip_end,
+max(STRFTIME('%s', t.start_time), STRFTIME('%s', DATE(dr.x, 'unixepoch'))) as left_time_point,
+min(STRFTIME('%s', t.end_time), STRFTIME('%s', DATE(dr.x, 'unixepoch', '+1 day'))) as right_time_point,
+max(min(STRFTIME('%s', t.end_time), STRFTIME('%s', DATE(dr.x, 'unixepoch', '+1 day'))) - max(STRFTIME('%s', t.start_time), STRFTIME('%s', DATE(dr.x, 'unixepoch'))), 0) as diff
+from days_range dr, trip t
+WHERE t.bike_id <= 100 and diff > 0;
